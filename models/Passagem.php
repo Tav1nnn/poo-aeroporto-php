580305cae8b39dd20a9550ac8ed6cef7f6bee03f
Otavio Marques <marquesotavio251204@gmail.com>
2024-02-29 20:14:30 -0300

    encapsulando classe passagem

--- models/Passagem.php
+++ models/Passagem.php
@@ -4,12 +4,45 @@ require_once 'Voo.php';
 require_once 'Usuario.php';
 
 class Passagem {
-    public string $codBilhete;
-    public float $valor;
-    public Voo $voo;
-    public Usuario $usuario;
+    private string $codBilhete;
+    private float $valor;
+    private Voo $voo;
+    private Usuario $usuario;
 
     public function __construct() {
-        
+
+    }
+
+     public function getCodBilhete(): string {
+        return $this->codBilhete;
+    }
+
+   
+    public function setCodBilhete(string $codBilhete): void {
+        $this->codBilhete = $codBilhete;
+    }
+
+    public function getValor(): float {
+        return $this->valor;
+    }
+
+    public function setValor(float $valor): void {
+        $this->valor = $valor;
+    }
+
+    public function getVoo(): Voo {
+        return $this->voo;
+    }
+
+    public function setVoo(Voo $voo): void {
+        $this->voo = $voo;
+    }
+
+    public function getUsuario(): Usuario {
+        return $this->usuario;
+    }
+
+    public function setUsuario(Usuario $usuario): void {
+        $this->usuario = $usuario;
     }
 }
