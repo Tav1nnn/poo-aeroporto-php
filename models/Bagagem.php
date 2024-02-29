2bb98bcc097dbe347b94253868f46a820e1d2f44
Otavio Marques <marquesotavio251204@gmail.com>
2024-02-29 20:11:15 -0300

    encapsulando classe bagagem

--- models/Bagagem.php
+++ models/Bagagem.php
@@ -3,7 +3,35 @@
 require_once 'Usuario.php';
 
 class Bagagem {
-    public float $peso;
-    public int $numBagagem;
-    public Usuario $usuario;
+    private float $peso;
+    private int $numBagagem;
+    private Usuario $usuario;
+
+    function __construct () {
+        
+    }
+
+    public function getPeso(): float {
+        return $this->peso;
+    }
+
+    public function setPeso(float $peso): void {
+        $this->peso = $peso;
+    }
+
+    public function getNumBagagem(): int {
+        return $this->numBagagem;
+    }
+
+    public function setNumBagagem(int $numBagagem): void {
+        $this->numBagagem = $numBagagem;
+    }
+
+    public function getUsuario(): Usuario {
+        return $this->usuario;
+    }
+
+    public function setUsuario(Usuario $usuario): void {
+        $this->usuario = $usuario;
+    }
 }
