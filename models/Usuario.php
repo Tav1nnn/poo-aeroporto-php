b86bab480941ee52bba2a35ce1d9adfc00e5f67d
Otavio Marques <marquesotavio251204@gmail.com>
2024-02-29 20:16:19 -0300

    encapsulando classe usuario

--- models/Usuario.php
+++ models/Usuario.php
@@ -1,11 +1,35 @@
 <?php
 
 class Usuario {
-    public string $nome;
-    public string $email;
-    public string $cpf;
+    private string $nome;
+    private string $email;
+    private string $cpf;
 
     public function __construct() {
         
+    }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): void {
+        $this->nome = $nome;
+    }
+
+    public function getEmail(): string {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void {
+        $this->email = $email;
+    }
+
+    public function getCpf(): string {
+        return $this->cpf;
+    }
+
+    public function setCpf(string $cpf): void {
+        $this->cpf = $cpf;
     }
 }
