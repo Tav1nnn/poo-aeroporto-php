8ce64162116145e611d9f97c8b39ad8dad60b326
Otavio Marques <marquesotavio251204@gmail.com>
2024-02-29 19:39:39 -0300

    encapsulando classe tripulante

--- models/Tripulante.php
+++ models/Tripulante.php
@@ -2,13 +2,35 @@
 
 class Tripulante {
     
-    public String $nome;
-    public String $licenca;
-    public String $cargo;
+    private String $nome;
+    private String $licenca;
+    private String $cargo;
 
-    public function __construct($nome, $licenca, $cargo) {
+    public function __construct () {
+        
+    }
+
+    public function setNome (string $nome):void {
         $this->nome = $nome;
+    }
+
+    public function getNome ():string {
+        return $this->nome;
+    }
+
+    public function setLicenca (string $licenca):void {
         $this->licenca = $licenca;
+    }
+
+    public function getLicenca ():string {
+        return $this->licenca;
+    }
+
+    public function setCargo (string $cargo):void {
         $this->cargo = $cargo;
+    }
+
+    public function getCargo ():string {
+        return $this->cargo;
     }
 }
