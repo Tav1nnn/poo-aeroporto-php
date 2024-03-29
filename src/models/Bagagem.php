8ad7a0c02ef91563cfd3e2dd199c0080fa4acf8c
Otavio Marques <marquesotavio251204@gmail.com>
2024-03-29 10:56:22 -0300

    finalizando logica do bagagem

--- src/models/Bagagem.php
+++ src/models/Bagagem.php
@@ -2,36 +2,41 @@
 
 namespace otavio\PooAeroportoPhp;
 
-class Bagagem {
+class Bagagem
+{
     private float $peso;
     private int $numBagagem;
     private Usuario $usuario;
 
-    function __construct () {
-        
+    public function __construct(float $peso, int $numBagagem, Usuario $usuario)
+    {
+        $this->peso = $peso;
+        $this->numBagagem = $numBagagem;
+        $this->usuario = $usuario;
     }
 
-    public function getPeso(): float {
+    public function getPeso(): float
+    {
         return $this->peso;
     }
 
-    public function setPeso(float $peso): void {
+    public function setPeso(float $peso): void
+    {
         $this->peso = $peso;
     }
 
-    public function getNumBagagem(): int {
+    public function getNumBagagem(): int
+    {
         return $this->numBagagem;
     }
 
-    public function setNumBagagem(int $numBagagem): void {
+    public function setNumBagagem(int $numBagagem): void
+    {
         $this->numBagagem = $numBagagem;
     }
 
-    public function getUsuario(): Usuario {
+    public function getUsuario(): Usuario
+    {
         return $this->usuario;
-    }
-
-    public function setUsuario(Usuario $usuario): void {
-        $this->usuario = $usuario;
     }
 }
