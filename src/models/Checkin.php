49a0a83f8a6bccb24e2cb6410f91342545fc51d3
Otavio Marques <marquesotavio251204@gmail.com>
2024-03-29 10:59:52 -0300

    finalizando logica da passagem e cheking

--- src/models/Checkin.php
+++ src/models/Checkin.php
@@ -6,27 +6,23 @@ class Checkin {
     private Usuario $usuario;
     private Passagem $passagem;
 
-    public function __construct() {
-        
+    public function __construct(Usuario $usuario, Passagem $passagem) {
+        $this->validar($usuario, $passagem);
     }
 
-    public function validar($usuario, $passagem) {
-        
+    private function validar(Usuario $usuario, Passagem $passagem) {
+        if($passagem->getUsuario() == $usuario)
+        {
+            $this->usuario = $usuario;
+            $this->passagem = $passagem;
+        }
     }
 
     public function getUsuario(): Usuario {
         return $this->usuario;
     }
 
-    public function setUsuario(Usuario $usuario): void {
-        $this->usuario = $usuario;
-    }
-
     public function getPassagem(): Passagem {
         return $this->passagem;
-    }
-
-    public function setPassagem(Passagem $passagem): void {
-        $this->passagem = $passagem;
     }
 }
--- src/models/Passagem.php
+++ src/models/Passagem.php
@@ -2,46 +2,53 @@
 
 namespace otavio\PooAeroportoPhp;
 
-class Passagem {
+class Passagem
+{
     private string $codBilhete;
     private float $valor;
     private Voo $voo;
     private Usuario $usuario;
 
-    public function __construct() {
-
+    public function __construct(string $codBilhete, float $valor, Voo $voo, Usuario $usuario)
+    {
+        $this->codBilhete = $codBilhete;
+        $this->valor = $valor;
+        $this->voo = $voo;
+        $this->usuario = $usuario;
     }
 
-     public function getCodBilhete(): string {
+    public function getCodBilhete(): string
+    {
         return $this->codBilhete;
     }
 
-   
-    public function setCodBilhete(string $codBilhete): void {
+    public function setCodBilhete(string $codBilhete): void
+    {
         $this->codBilhete = $codBilhete;
     }
 
-    public function getValor(): float {
+    public function getValor(): float
+    {
         return $this->valor;
     }
 
-    public function setValor(float $valor): void {
+    public function setValor(float $valor): void
+    {
         $this->valor = $valor;
     }
 
-    public function getVoo(): Voo {
+    public function getVoo(): Voo
+    {
         return $this->voo;
     }
 
-    public function setVoo(Voo $voo): void {
+    public function setVoo(Voo $voo): void
+    {
         $this->voo = $voo;
     }
 
-    public function getUsuario(): Usuario {
+    public function getUsuario(): Usuario
+    {
         return $this->usuario;
-    }
-
-    public function setUsuario(Usuario $usuario): void {
-        $this->usuario = $usuario;
     }
 }
